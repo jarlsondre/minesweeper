acebd7d8f4757e9357ef7dfb4c54a36a7693b240
Jarl Sondre Sæther <jssaethe@stud.ntnu.no>
2021-02-21 15:26:00 +0100

    Removed template files, added own files

--- src/main/java/minesweeper/fxui/App.java
+++ src/main/java/minesweeper/fxui/App.java
@@ -2,7 +2,6 @@ package minesweeper.fxui;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -13,22 +12,11 @@ import java.io.IOException;
  */
 public class App extends Application {
 
-    private static Scene scene;
-
     @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+    public void start(final Stage stage) throws IOException {
+        Scene scene = new Scene(new FXMLLoader(App.class.getResource("startScreen.fxml")).load());
         stage.setScene(scene);
         stage.show();
-    }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/minesweeper/fxui/MainController.java
@@ -0,0 +1,11 @@
+package minesweeper.fxui;
+
+import javafx.fxml.FXML;
+
+public class MainController {
+
+    @FXML
+    private void handleButton() {
+
+    }
+}
