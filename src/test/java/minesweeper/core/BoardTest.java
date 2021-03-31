8f772890541dbff0e2f1d5ba3fc6e44cde7a105e
Jarl Sondre Sæther <jssaethe@stud.ntnu.no>
2021-03-31 15:21:56 +0200

    la til testfiler

---
+++ src/test/java/minesweeper/core/BoardTest.java
@@ -0,0 +1,11 @@
+package minesweeper.core;
+
+import org.junit.jupiter.api.Test;
+
+public class BoardTest {
+
+    @Test
+    public void testNothing() {
+        System.out.println("lol");
+    }
+}
